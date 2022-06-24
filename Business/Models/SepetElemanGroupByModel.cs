1350efd98b393aaab6679d3b538dea132a0767bd
Oğuz Kağan Koç <oguzkagan.koc@bilgeadamakademi.com>
2022-06-24 09:28:28 +0300

    Add project files.

---
+++ Business/Models/SepetElemanGroupByModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace Business.Models
+{
+    public class SepetElemanGroupByModel
+    {
+        public int UrunId { get; set; }
+        public int KullaniciId { get; set; }
+
+        [DisplayName("Ürün Adı")]
+        public string UrunAdi { get; set; }
+
+        [DisplayName("Toplam Birim Fiyat")]
+        public string ToplamBirimFiyatiDisplay { get; set; }
+
+        public double ToplamBirimFiyati { get; set; }
+
+        [DisplayName("Ürün Sayısı")]
+        public int UrunSayisi { get; set; }
+    }
+}
---
+++ Business/Models/SepetElemanModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Business.Models
+{
+    public class SepetElemanModel
+    {
+        public int UrunId { get; set; }
+        public int KullaniciId { get; set; }
+        public string UrunAdi { get; set; }
+        public double BirimFiyati { get; set; }
+    }
+}
